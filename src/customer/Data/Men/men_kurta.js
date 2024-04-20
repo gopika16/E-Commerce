a9934cba954bb87081e442ee40008690bd0b227c
Sakshi Chopra <sakshichopra3010@gmail.com>
2024-04-20 18:12:08 +0530

    Data folder

---
+++ src/customer/Data/Men/men_kurta.js
@@ -0,0 +1,17 @@
+export const menKurta=[
+    {
+        'image':'',
+        'brand': 'Majestic Man',
+        'title': 'Men Printed Pure Cotton Straight Kurta',
+        'color': 'Green',
+        'discountedPrince': 499,
+        'originalPrice': 1499,
+        'discountPercent': 66,
+        'size': [32, 34, 36, 38],
+        'quantity': 100,
+        'topLevelCategory': 'Men',
+        'secondLevelCategory': 'Clothing',
+        'thirdLevelCategory': 'menKurta',
+        'description': 'A traditional garment embodying elegance and grace. Crafted from fine fabric. Made for comfort.'
+    }
+]
