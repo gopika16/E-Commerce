6de847549321bac4f304dcb81aac06c477a8d9bc
Gopika <51948096+gopika16@users.noreply.github.com>
2024-04-20 22:08:08 +0530

    Added products page

--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import MainCarousel from '../../components/HomeCarousel/MainCarousel'
 import ProductsCarousel from '../../components/HomeProductCardsCarousel/ProductsCarousel'
+import ProductCard from '../../components/Product/ProductCard'
+import Product from '../../components/Product/Product'
 
 const HomePage = () => {
   return (
     <div>
-        <MainCarousel/>
+        {/* <MainCarousel/> */}
         <div>
-            <ProductsCarousel/>
+            {/* <ProductsCarousel/> */}
+            {/* <ProductCard/> */}
+            <Product/>
         </div>
     </div>
   )
