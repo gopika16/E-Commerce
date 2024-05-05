0d438a4a95a119c3fd972d9fc3200c4202a531ff
Gopika <51948096+gopika16@users.noreply.github.com>
2024-05-05 17:00:23 +0530

    Added auth controller and response codes

--- API/src/controller/auth.controller.js
+++ API/src/controller/auth.controller.js
@@ -1,1 +1,34 @@
-const userService = require("../services/user.service");
+const userService = require("../services/user.service.js");
+const jwtProvider = require("../config/jwtProvider.js");
+const responseCode = require("../HttpResponse.js");
+
+const register = async (req, res) => {
+    try {
+        const user = await userService.createUser(req.body);
+        const jwt = jwtProvider.generateToken(user._id);
+
+        // await CartService.createCart(user);
+
+        return res.status(200).send({ jwt, message: "Registration Successful !!" });
+
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+};
+
+const login = async (req, res) => {
+
+    const { password, email } = req.body;
+
+    try {
+
+        const user = await userService.getUserByEmail(email);
+
+        if (!user) {
+            return res.status(responseCode.NOT_FOUND).send({ message: " User not found." });
+        }
+    } catch (error) {
+        return res.status(500).send({ error: error.message });
+    }
+};
+
