620915654c352f810efcc44fabc361fe08abb598
Sakshi Chopra <sakshichopra3010@gmail.com>
2024-05-05 16:26:07 +0530

    Added userByEmail, getAllUser (service methods)

--- API/src/services/user.service.js
+++ API/src/services/user.service.js
@@ -3,53 +3,78 @@ const bcrypt = require("bcrypt");
 const jwtProvider = require("../config/jwtProvider.js");
 
 const createUser = async (userData) => {
-    try {
-        let { firstName, lastName, email, password } = userData;
+  try {
+    let { firstName, lastName, email, password } = userData;
 
-        const isUserExist = await User.findOne({ email });
+    const isUserExist = await User.findOne({ email });
 
-        if (isUserExist) {
-            throw new Error("Email is already Registered!");
-        }
-
-        password = await bcrypt.hash(password, 8);
-
-        const user = await User.create({ firstName, lastName, email, password });
-
-        console.log("New user created", user);
-
-        return user;
-
-    } catch (error) {
-        throw new Error(error.message);
+    if (isUserExist) {
+      throw new Error("User already Exists!");
     }
+
+    password = await bcrypt.hash(password, 8);
+
+    const user = await User.create({ firstName, lastName, email, password });
+
+    console.log("New user created", user);
+
+    return user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 const findUserById = async (userId) => {
-    try {
-        const user = await User.findById(userId).populate("address");
-        if (!user) {
-            throw new Error("User not found with Id : ", userId);
-        }
-        return user;
-    } catch (error) {
-        throw new Error(error.message);
+  try {
+    const user = await User.findById(userId).populate("address");
+    if (!user) {
+      throw new Error("User not found with Id : ", userId);
     }
+    return user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
 const getUserProfileByToken = async (token) => {
-    try {
-        const userId = await jwtProvider.getUserIdFromToken(token);
+  try {
+    const userId = await jwtProvider.getUserIdFromToken(token);
 
-        const user = await findUserById(userId);
+    const user = await findUserById(userId);
 
-        if (!user) {
-            throw new Error("User not found: ", userId);
-        }
-        return user;
-    } catch (error) {
-        throw new Error(error.message);
+    if (!user) {
+      throw new Error("User not found: ", userId);
     }
+    return user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
 };
 
-module.exports = { createUser, findUserById, getUserProfileByToken };
+const findUserByEmail = async (email) => {
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      throw new Error("User does not exist with email " + email);
+    }
+    return user;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const getAllUsers = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+module.exports = {
+  createUser,
+  findUserById,
+  getUserProfileByToken,
+  findUserByEmail,
+  getAllUsers,
+};
