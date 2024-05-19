7817316b17b495b79348390c3a05fc2c97833331
Sakshi Chopra <sakshichopra3010@gmail.com>
2024-05-19 14:26:28 +0530

    updated product.service.js

--- API/src/services/product.service.js
+++ API/src/services/product.service.js
@@ -1,7 +1,57 @@
 const Category = require("../models/category.model");
+const Product = require("../models/product.model");
 
 async function createProduct(reqData) {
-    const topLevel = await Category.findOne({
+    let topLevel = await Category.findOne({
         name: reqData.topLevelCategory
-    })
+    });
+
+    if(!topLevel) {
+        topLevel = new Category({
+            name: reqData.topLevelCategory,
+            level: 1
+        });
+    }
+
+    let secondLevel = await Category.findOne({
+        name: reqData.secondLevelCategory,
+        parentCategory: topLevel._id,
+    });
+
+    if(!secondLevel) {
+        secondLevel = new Category({
+            name: reqData.secondLevelCategory,
+            parentCategory: topLevel._id,
+            level: 2
+        });
+    }
+
+    let thirdLevel = await Category.findOne({
+        name: reqData.thirdLevelCategory,
+        parentCategory: secondLevel._id
+    });
+
+    if(!thirdLevel) {
+        thirdLevel = new Category({
+            name: reqData.thirdLevelCategory,
+            parentCategory: secondLevel._id,
+            level: 3
+        });
+    }
+
+    const product = new Product({
+        title: reqData.title,
+        color: reqData.color,
+        description: reqData.description,
+        discountedPrice: reqData.discountedPrice,
+        discountPercent: reqData.discountPercent,
+        imageUrl: reqData.imageUrl,
+        brand: reqData.brand,
+        price: reqData.price,
+        sizes: reqData.sizes,
+        quantity: reqData.quantity,
+        category: thirdLevel._id
+    });
+
+    return await product.save();
 }
